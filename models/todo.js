1da41e801e5098e5231fcf10a734e55c2afe6c97
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-10-08 00:23:55 +0700

    add comment to todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,17 +1,21 @@
+// Import the mongoose module
 const mongoose = require('mongoose')
 
+// Set up default mongoose connection
 const dbUrl = 'mongodb://localhost:27017/todoDB'
 mongoose.connect(dbUrl,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).catch(err=>console.log(err))
 
+// Define a schema
 let todoSchema = mongoose.Schema({
     title:String,
     datetime:String,
     text:String
 })
 
+// Compile model from schema
 let Todo = mongoose.model("todo", todoSchema)
 
 module.exports = Todo
